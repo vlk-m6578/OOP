b74fd8aee7f1b7bb3add3d347d066e19888ff6af
Milana L <lebedeva310@icloud.com>
2025-03-11 00:47:34 +0300

    update move and clone triangle

--- LR1/Models/Triangle.cs
+++ LR1/Models/Triangle.cs
@@ -17,21 +17,33 @@ namespace PAINT.Models
             {
                 return;
             }
-
         }
         public Triangle() { }
         public override void Draw(int w, int h)
         {
-
+            
         }
         
         public override void Move(int deltaX, int deltaY)
         {
-            
+            x1 += deltaX;
+            y1 += deltaY;
+            x2 += deltaX;
+            y2 += deltaY;
+            x3 += deltaX;
+            y3 += deltaY;
         }
         public override Shape Clone()
         {
-           
+            return new Triangle(this.Name, this.Symbol, this.SymbolBackground, 0, 0, 0)
+            {
+                x1=this.x1,
+                y1=this.y1,
+                x2=this.x2,
+                y2=this.y2,
+                x3=this.x3,
+                y3=this.y3
+            };
         }
     }
 }
