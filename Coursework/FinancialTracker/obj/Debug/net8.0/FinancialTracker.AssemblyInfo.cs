323c73a1e6d72129acbd65ee132e773f32a1383a
Milana L <lebedeva310@icloud.com>
2025-04-18 13:03:28 +0300

    jjd

--- Coursework/FinancialTracker/obj/Debug/net8.0/FinancialTracker.AssemblyInfo.cs
+++ Coursework/FinancialTracker/obj/Debug/net8.0/FinancialTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinancialTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ab08068c58126583a97fcdf847309dd5f36ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7fb9b0efac37e12c3b4da29451b328507f6f0cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinancialTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinancialTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
