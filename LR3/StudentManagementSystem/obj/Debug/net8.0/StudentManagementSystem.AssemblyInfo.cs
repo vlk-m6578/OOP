62a4151e9df616dd94eac800c791ff1ff0b21ba2
Milana L <lebedeva310@icloud.com>
2025-05-03 13:16:46 +0300

    create repository

--- LR3/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ LR3/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2128cb1181900620bd01ce996be8cf656eccdc3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40fdba69eceadb9b6fbe36658936e4c6bf6fb961")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
